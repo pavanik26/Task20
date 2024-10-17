8a3675a955dfcc5578ef9f75e563f46507597cc5
pavanik26 <pavani.kashana26@gmail.com>
2024-10-17 16:43:42 +0530

    initial commit

---
+++ src/main/java/jatTask20/jatTask20/App.java
@@ -0,0 +1,13 @@
+package jatTask20.jatTask20;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
